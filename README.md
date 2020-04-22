943ace7e120711b8cb6898cbd9dea10b90dcfecc
Victor <victorsinkunas@gmail.com>
2020-04-22 10:49:19 +0200

    ignore node_modules, add mongodb

--- README.md
+++ README.md
@@ -4,4 +4,6 @@
 
 - Please install node and make sure it is installed by running: ``` node -v```
 
+mongoDB: dbUser/dbUser
+
 You good to go :)
--- server.js
+++ server.js
@@ -1,12 +1,24 @@
-console.log('May node be with you')
+console.log('May node be with you');
+const client = require('mongodb').MongoClient;
+const url = "mongodb+srv://dbUser:dbUser@cluster0-4b6gs.mongodb.net/test?retryWrites=true&w=majority";
 const express = require('express');
+const bParser = require('body-parser');
 const app = express();
 
+app.use(bParser.urlencoded({ extended: true }))
+
 app.listen(3000, function() {
-	console.log('listening on 3000')
-})
+	console.log('listening on 3000');
+});
 
-app.get('/', (req,res) => {
-	res.sendFile(__dirname + '/index.html')
-})
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/index.html');
+});
+app.post('/quotes', (req, res) => {
+	console.log(req.body);
+});
 
+client.connect(url, { useUnifiedTopology: true })
+	.then(client => {
+		console.log('Connected to Database');
+	}).catch( error => console.error(error))
